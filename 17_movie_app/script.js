388858595e483699c0d513f1c076cfd168e13e7b
Vahe Abovyan <VAbovyan@beeline.am>
2021-10-11 18:31:37 +0400

    movie app half project

---
+++ 17_movie_app/script.js
@@ -0,0 +1,4 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6219d46a85c959170353e7406f7a4b08&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=6219d46a85c959170353e7406f7a4b08&query="';
+
