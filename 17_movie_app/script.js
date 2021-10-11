54d399e16371ac292ed18c4f67a45fde70a68af1
Vahe Abovyan <VAbovyan@beeline.am>
2021-10-11 19:02:04 +0400

    new

--- 17_movie_app/script.js
+++ 17_movie_app/script.js
@@ -1,4 +1,38 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6219d46a85c959170353e7406f7a4b08&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=6219d46a85c959170353e7406f7a4b08&query="';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=6219d46a85c959170353e7406f7a4b08&query="';
 
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
+
+// get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+    main.innerHTML = '';
+
+    movies.forEach(movie => {
+        const { title, poster_path, vote_average, overview } = movie;
+    });
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm);
+
+        search.value = '';
+    } else {
+        window.location.reload();
+    }
+});
